14ab1061a3a1a5a2c5189e2253713bf9a941e08a
bosphere <boss.army@gmail.com>
2016-01-03 16:51:01 +0800

    introduce intermediate data structure `SlideItem` to prevent children views spiking up

--- app/src/main/java/com/example/bo/slidinglayoutdemo/MainActivity.java
+++ app/src/main/java/com/example/bo/slidinglayoutdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.ImageSpan;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -34,13 +35,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addTextView() {
-        TextView tv = new TextView(MainActivity.this);
-        SpannableStringBuilder ssb = new SpannableStringBuilder("text" + mRnd.nextLong());
-        ssb.setSpan(new ImageSpan(this, android.R.drawable.presence_audio_away), ssb.length() - 1, ssb.length(),
-                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        tv.setText(ssb);
-        tv.setTextColor(Color.WHITE);
-        tv.setTextSize(10 + mRnd.nextInt(10));
-        mSlidingLayout.addView(tv);
+        mSlidingLayout.enqueue(new SlidingLayout.SlideItem() {
+            @Override
+            protected View onCreateView() {
+                TextView tv = new TextView(MainActivity.this);
+                SpannableStringBuilder ssb = new SpannableStringBuilder("text" + mRnd.nextLong());
+                ssb.setSpan(new ImageSpan(MainActivity.this, android.R.drawable.presence_audio_away), ssb.length() - 1,
+                        ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                tv.setText(ssb);
+                tv.setTextColor(Color.WHITE);
+                tv.setTextSize(10 + mRnd.nextInt(10));
+                return tv;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bo/slidinglayoutdemo/SlidingLayout.java
+++ app/src/main/java/com/example/bo/slidinglayoutdemo/SlidingLayout.java
@@ -9,15 +9,15 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by bo on 25/12/15.
  */
 public class SlidingLayout extends ViewGroup {
 
-    private final Set<View> mNewViews = new HashSet<>();
+    private final Queue<SlideItem> mQueue = new LinkedList<>();
     private int mFirstChildLeft;
     private View mFirstChild;
     private int mVelocity;
@@ -50,88 +50,23 @@ public class SlidingLayout extends ViewGroup {
                 getResources().getDisplayMetrics());
     }
 
+    public void enqueue(SlideItem item) {
+        if (item == null) {
+            throw new IllegalArgumentException("Slide item cannot be null");
+        }
+        item.state = SlideItem.QUEUING;
+        mQueue.add(item);
+        if (getChildCount() == 0) {
+            appendNext();
+        }
+    }
+
     public void setVelocity(int velocity) {
         mVelocity = velocity;
     }
 
     public void setDividerSize(int size) {
         mDividerSize = size;
-    }
-
-    private void preAddView(View child) {
-        mNewViews.add(child);
-    }
-
-    @Override
-    public void addView(View child) {
-        preAddView(child);
-        super.addView(child);
-    }
-
-    @Override
-    public void addView(View child, int index) {
-        preAddView(child);
-        super.addView(child, index);
-    }
-
-    @Override
-    public void addView(View child, int index, LayoutParams params) {
-        preAddView(child);
-        super.addView(child, index, params);
-    }
-
-    @Override
-    public void addView(View child, LayoutParams params) {
-        preAddView(child);
-        super.addView(child, params);
-    }
-
-    @Override
-    public void addView(View child, int width, int height) {
-        preAddView(child);
-        super.addView(child, width, height);
-    }
-
-    @Override
-    public void removeView(View view) {
-        super.removeView(view);
-        postRemoveView(view);
-    }
-
-    @Override
-    public void removeViewAt(int index) {
-        View view = getChildAt(index);
-        super.removeViewAt(index);
-        postRemoveView(view);
-    }
-
-    @Override
-    public void removeViews(int start, int count) {
-        View[] removedViews = new View[count];
-        for (int i = start; i < count; i++) {
-            removedViews[i] = getChildAt(i);
-        }
-        super.removeViews(start, count);
-        for (View v : removedViews) {
-            postRemoveView(v);
-        }
-    }
-
-    @Override
-    public void removeAllViews() {
-        int count = getChildCount();
-        View[] removedViews = new View[count];
-        for (int i = 0; i < count; i++) {
-            removedViews[i] = getChildAt(i);
-        }
-        super.removeAllViews();
-        for (View v : removedViews) {
-            postRemoveView(v);
-        }
-    }
-
-    private void postRemoveView(View view) {
-        mNewViews.remove(view);
     }
 
     @Override
@@ -180,6 +115,7 @@ public class SlidingLayout extends ViewGroup {
     private void doChildrenLayout() {
         int count = getChildCount();
         if (count == 0) {
+            appendNext();
             return;
         }
 
@@ -193,14 +129,29 @@ public class SlidingLayout extends ViewGroup {
             mFirstChild = first;
         }
 
+        int vw = getMeasuredWidth();
+        int vh = getMeasuredHeight();
+
         int left = getPaddingLeft() + mFirstChildLeft;
         boolean deleteFirstChild = false;
         for (int i = 0; i < count; i++) {
+            View child = getChildAt(i);
+            Object tag = child.getTag();
+            SlideItem item = null;
+            if (tag instanceof SlideItem) {
+                item = (SlideItem) tag;
+            }
+
+            if (item == null || (item.state != SlideItem.DISPLAYING && item.state != SlideItem.NEW)) {
+                // invalid child view, ignore
+                child.layout(0, 0, 0, 0);
+                continue;
+            }
+
             if (i > 0) {
                 left += mDividerSize;
             }
 
-            View child = getChildAt(i);
             MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
             left += lp.leftMargin;
             int w = child.getMeasuredWidth();
@@ -208,16 +159,16 @@ public class SlidingLayout extends ViewGroup {
                 deleteFirstChild = true;
             }
 
-            if (mNewViews.contains(child)) {
-                mNewViews.remove(child);
-                if (left >= 0 && left < getMeasuredWidth()) {
-                    lp.leftMargin += getMeasuredWidth() - left;
-                    left = getMeasuredWidth();
+            if (item.state == SlideItem.NEW) {
+                item.state = SlideItem.DISPLAYING;
+                if (left >= 0 && left < vw) {
+                    lp.leftMargin += vw - left;
+                    left = vw;
                 }
             }
 
             // try to center child vertically
-            int h = getMeasuredHeight() - getPaddingTop() - getPaddingBottom();
+            int h = vh - getPaddingTop() - getPaddingBottom();
             int ch = child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
             int extraTopPadding = 0;
             if (ch < h) {
@@ -236,10 +187,43 @@ public class SlidingLayout extends ViewGroup {
 
         if (deleteFirstChild) {
             // first child already out of sight, release
-            removeViewAt(0);
+            releaseFirst();
         } else {
             mFirstChildLeft -= mVelocity;
         }
+
+        if (left <= vw) {
+            appendNext();
+        }
+    }
+
+    private void releaseFirst() {
+        View first = getChildAt(0);
+        if (first == null) {
+            return;
+        }
+        Object tag = first.getTag();
+        if (tag instanceof SlideItem) {
+            SlideItem item = (SlideItem) tag;
+            item.state = SlideItem.REMOVED;
+        }
+        removeViewAt(0);
+    }
+
+    private void appendNext() {
+        SlideItem item = mQueue.poll();
+        if (item == null || item.state != SlideItem.QUEUING) {
+            return;
+        }
+        View view = item.onCreateView();
+        if (view == null) {
+            return;
+        }
+
+        item.state = SlideItem.NEW;
+
+        view.setTag(item);
+        super.addView(view);
     }
 
     @Override
@@ -260,5 +244,17 @@ public class SlidingLayout extends ViewGroup {
     @Override
     protected boolean checkLayoutParams(ViewGroup.LayoutParams p) {
         return p instanceof MarginLayoutParams;
+    }
+
+    public static abstract class SlideItem {
+
+        private static final int QUEUING    = 1;
+        private static final int NEW        = 2;
+        private static final int DISPLAYING = 3;
+        private static final int REMOVED    = 4;
+
+        private int state;
+
+        protected abstract View onCreateView();
     }
 }
